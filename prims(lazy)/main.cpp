dc79b1371d959d968889c2fcc85a1db7b8c12edd
Vidush <vidush23592@iiitd.ac.in>
2024-04-29 17:01:46 +0530

    Adding little code to prims, added priority queue.

--- prims(lazy)/main.cpp
+++ prims(lazy)/main.cpp
@@ -1,14 +1,100 @@
 //
 //  main.cpp
-//  prims(lazy)
+//  BFS
 //
 //  Created by Vidush Jindal on 27/04/24.
 //
 
 #include <iostream>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <queue>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+using namespace std;
+
+struct Node{
+    int data=-1;
+};
+
+struct Edge{
+    Node* from;
+    Node* to;
+    int cost;
+    
+    Edge(Node* from, Node* to, int cost){
+        this->from=from;
+        this->to=to;
+        this->cost=cost;
+        
+    }
+};
+
+struct edge_compare{
+    bool operator()(Edge a, Edge b){
+        return a.cost>b.cost;
+    }
+};
+
+void add_directed_edge_(map<Node* , vector<Edge> > &graph, Node* from, Node* to, int cost ){
+    vector<Edge> list = graph[from];
+    
+    if (list.empty()) graph[from]= *(new vector<Edge>);
+                     
+    Edge new_edge=Edge(from, to, cost);
+    graph[from].push_back(new_edge);
+}
+
+struct Graph{
+    vector<Node *> nodes;
+    map<Node*, vector<Edge>> graph;
+    
+    Node* create_node(int nd){
+        Node* from_node=0;
+        for (Node* node: nodes){
+            if (node->data==nd) {
+                from_node=node;
+                break;}
+        }
+        
+        if (not from_node) from_node=new Node;
+        from_node->data=nd;
+        return from_node;
+    }
+    
+    void add_directed_edge(int from, int to, int cost){
+        Node* from_node=create_node(from);
+        Node* to_node=create_node(to);
+        
+        nodes.push_back(from_node);
+        nodes.push_back(to_node);
+        
+        add_directed_edge_(graph, from_node, to_node, cost);
+    }
+    
+    void add_undirected_edge(int from, int to, int cost){
+        add_directed_edge(from, to, cost);
+        add_directed_edge(to, from, cost);
+        
+    }
+    
+
+    
+    void clear(){
+        nodes.clear();
+        graph.clear();
+    }
+    
+    vector<Edge> prims(int start){
+        Node* start_node=create_node(start);
+        priority_queue <Edge, vector<Edge>, edge_compare> pq;
+        
+        int visited(nodes.size());
+    }
+};
+
+
+int main(){
+    
 }
--- priority_queue/main.cpp
+++ priority_queue/main.cpp
@@ -1,14 +1,138 @@
-//
-//  main.cpp
-//  priority_queue
-//
-//  Created by Vidush Jindal on 27/04/24.
-//
-
+#include <cstdio>
 #include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+void print_array(vector<int> x){
+    for (int i=0; i< x.size(); i++){
+        std::cout<<x[i]<<' ';
+    }
+    std::cout<<std::endl;
+
+}
+
+struct Heap{
+    vector<int> heap{};
+    int size=0;
+    
+    void heapify_up(int root){
+        while (root!=0){
+            int ind=(root-1)/2;
+            if (heap[ind]> heap[root]) {
+                int temp=heap[ind];
+                heap[ind]=heap[root];
+                heap[root]=temp;
+                
+                root=ind;
+                
+            }
+            else {break;}
+        }
+        
+    }
+
+    void heapify_down(int root){
+        int l=heap.size()-1; //off by one indicing
+        int ind=2*root+1;
+        while (ind <= l){
+            int no_small= heap[root];
+            int small=root;
+            if (heap[root]> heap[ind]) {no_small=heap[ind]; small=ind;}
+            
+            if (ind!=l) {
+                if (no_small>heap[ind+1]) {small=ind+1; no_small=heap[ind+1];}
+            }
+            
+            if (small== root) break;
+            else {
+                int temp= heap[root];
+                heap[root]=heap[small];
+                heap[small]=temp;
+                
+                root=small;
+                ind=2*small+1;
+                
+            }
+            
+        }
+    }
+
+    void insert(int d){
+//        for (int i=0; i<heap.size(); i++) {if (heap[i]==d) return;} //duplicate element
+        heap.push_back(d);
+        heapify_up(heap.size()-1);
+        
+
+    }
+
+    void del(int ind){
+        int l=heap.size();
+        heap[ind]=heap[l-1];
+        heap.pop_back();
+        heapify_down(ind);
+        heapify_up(ind);
+        
+        
+    }
+
+//    int min_extract(){
+//        if (heap.empty()) {cout<<"No values to return min.";return -1;}
+//        int min=heap[0];
+//        return min;
+//    }
+    
+    void make_sorted(Heap heap_struct, vector <int> &x){
+        while (not heap_struct.heap.empty()){
+            int no=heap_struct.min();
+            heap_struct.del(0);
+            x.push_back(no);
+        }
+        
+    }
+    
+    void update_size(){
+        this->size=heap.size();
+        
+    }
+    
+    int min(){
+        return heap[0];
+    }
+    
+    int max(){ //o(n)
+        int l=heap.size();
+        int n= ceil(log2(l+1));
+        int start=pow(2,n-1)-1;
+        int end=l-1;
+        int max=heap[start];
+        for (int i=start; i<=end;i++){
+            if (heap[i]>max) max=heap[i];
+        }
+        return max;
+    }
+    
+};
+
+
+int main(){
+    //testing the heap code
+/*    vector<int> arr={1,2,3,0,9,8,3,0,9,8,7,9,83,-1,-9,8,0,3,4,2,3,5,5,4,3,2,3,4,5,5};
+    Heap my_heap;
+    
+    for (int i=0; i<arr.size(); i++) my_heap.insert(arr[i]);
+    print_array(arr);
+    print_array(my_heap.heap);
+    
+    vector<int> heap_sorted;
+    my_heap.make_sorted(my_heap, heap_sorted);
+    
+    print_array(my_heap.heap);
+    print_array(heap_sorted);
+ */
+    
+    
+    
+    
+    
 }
