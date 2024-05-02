6febb27ddf5f96976ff60303ff216ef6777938bf
Vidush <vidush23592@iiitd.ac.in>
2024-05-02 16:17:01 +0530

    adding bfs shortest path from a node to other

---
+++ Algorithms_In_CPP/bfs_shortest_path/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bfs_shortest_path
+//
+//  Created by Vidush Jindal on 02/05/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
