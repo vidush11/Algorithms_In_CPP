55038019e36d7f0b76f1a7c3b79e1f763533ef44
Vidush <vidush23592@iiitd.ac.in>
2024-05-05 14:44:40 +0530

    added code for djijkstra

---
+++ Algorithms_In_CPP/djikstra_lazy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  djikstra_lazy
+//
+//  Created by Vidush Jindal on 05/05/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
