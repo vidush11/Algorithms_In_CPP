1c1223eea97c32f0a2bdcf369b724b34ecad7058
Vidush <vidush23592@iiitd.ac.in>
2024-05-10 12:45:49 +0530

    minor changes

---
+++ 2d_maxima/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2d_maxima
+//
+//  Created by Vidush Jindal on 10/05/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
