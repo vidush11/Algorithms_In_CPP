f413a0c15dcfa98ee52212f58ceb77609c5b14d3
Vidush <vidush23592@iiitd.ac.in>
2024-05-11 16:07:01 +0530

    added merge-sort and 2d maxima

---
+++ merge_sort/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  merge_sort
+//
+//  Created by Vidush Jindal on 10/05/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
